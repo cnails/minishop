5a9f33071c01b555130fc0003a90269c1d7ae74f
cnails <andreyandreevgr@gmail.com>
2020-06-07 21:52:52 +0300

    a

--- project/orders.php
+++ project/orders.php
@@ -2,8 +2,8 @@
     session_start();
     if ($_SESSION['loggued_on_user'] == "admin") {
         readfile("templates/header.html");
-        readfile("templates/orders.html");
         readfile("templates/admin.html");
+        readfile("templates/orders.html");
         readfile("templates/footer.html");
     } else {
         header("Location: index.php");
